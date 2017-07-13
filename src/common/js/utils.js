c28aa76186c2bdea7bd5965ec51e049bf5a89fff
yy9306 <1273460656@qq.com>
2017-07-13 17:54:20 +0800

    music

--- src/common/js/utils.js
+++ src/common/js/utils.js
@@ -12,3 +12,15 @@ export function shuffle (arr) {
   }
   return _arr
 }
+
+export function debounce (func, delay) {
+  let timer
+  return function (...arg) {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      func.apply(this, arg)
+    }, delay)
+  }
+}
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,14 +1,114 @@
 <template>
   <div class="search">
-    搜索
+    <div class="search-box-wrapper">
+       <search-box ref="searchBox" @query="OnQueryChange"></search-box>
+    </div>
+    <div class="shortcut-wrapper" v-show="!query">
+       <div class="shortcut">
+          <div class="hot-key">
+             <h1 class="title">热门搜索</h1>
+             <ul>
+               <li class="item" v-for="item in hotkey" @click="addQuery(item.k)">
+                 <span>{{item.k}}</span>
+               </li>
+             </ul>
+          </div>
+       </div>
+    </div>
+    <div class="search-result" v-show="query">
+      <suggest :query="query" @blurInput="changeBlur"></suggest>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-  export default{}
+import SearchBox from 'base/search-box/search-box'
+import {getHotKey} from 'api/search'
+import {ERR_OK} from 'api/config'
+import Suggest from 'components/suggest/suggest'
+export default{
+  components: {
+    SearchBox, Suggest
+  },
+  data () {
+    return {
+      hotkey: [],
+      query: ''
+    }
+  },
+  created () {
+    this._getHotKey()
+  },
+  methods: {
+    _getHotKey () {
+      getHotKey().then((res) => {
+        if (res.code === ERR_OK) {
+          this.hotkey = res.data.hotkey.slice(0, 10)
+        }
+      })
+    },
+    changeBlur () {
+      this.$refs.searchBox.blur()
+    },
+    addQuery (item) {
+      this.$refs.searchBox.setQuery(item)
+    },
+    OnQueryChange (query) {
+      this.query = query
+    }
+  }
+}
 </script>
 
 <style lang="stylus" scoped>
-  .search
-    color: #fff
+@import "~common/stylus/variable"
+@import "~common/stylus/mixin"
+
+.search
+  .search-box-wrapper
+    margin: 20px
+  .shortcut-wrapper
+    position: fixed
+    top: 178px
+    bottom: 0
+    width: 100%
+    .shortcut
+      height: 100%
+      overflow: hidden
+      .hot-key
+        margin: 0 20px 20px 20px
+        .title
+          margin-bottom: 20px
+          font-size: $font-size-medium
+          color: $color-text-l
+        .item
+          display: inline-block
+          padding: 5px 10px
+          margin: 0 20px 10px 0
+          border-radius: 6px
+          background: $color-highlight-background
+          font-size: $font-size-medium
+          color: $color-text-d
+      .search-history
+        position: relative
+        margin: 0 20px
+        .title
+          display: flex
+          align-items: center
+          height: 40px
+          font-size: $font-size-medium
+          color: $color-text-l
+          .text
+            flex: 1
+          .clear
+            extend-click()
+            .icon-clear
+              font-size: $font-size-medium
+              color: $color-text-d
+  .search-result
+    position: fixed
+    width: 100%
+    top: 178px
+    bottom: 0
 </style>
